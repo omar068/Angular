32478f5bbf6ffe017de318facb70889c6e7c8686
Omar <omar6688.oc@gmail.com>
2023-03-08 03:46:48 -0500

    Aprendiendo eventos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Omar'
+  age = 24;
+  img = "https://www.w3schools.com/w3images/avatar6.png";
+  btnDisable = true
+  person = {
+    name: 'Omar',
+    age: 24,
+    avatar : 'https://www.w3schools.com/w3images/avatar6.png'
+  }
+
+  toggleButton(){
+    // if (this.btnDisable == true){
+    //   this.btnDisable = false;
+    // } else{
+    //   this.btnDisable = true;
+    // }
+    //Forma de mejorarlo:
+    this.btnDisable = !this.btnDisable
+  }
+
+  ageIncrement(){
+    this.person.age++
+  }
+
+  onScroll(event : Event){
+    //el event.target es para que podamos obtener el elemento que está
+    //mandando el evento, y así acceder a sus propiedades
+    //Luego hacemos que se comporte como un elemento HTML
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+
+  }
+  changeName(event : Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
